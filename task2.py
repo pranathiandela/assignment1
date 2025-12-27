6a8731a8f350e8eb123b79b03656f532fa23694f
pranathiandela <pranathiandela@gmail.com>
2025-12-27 06:01:13 +0000

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,5 @@
+#create a personalized greeting
+f_name=input("Enter your first name: ")
+l_name=input("Enter your last name: ")
+concat=f_name+l_name
+print("Hello," ,concat, "! welcome to the python program.")
